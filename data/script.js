95a2d28a3ce2bcdc2afbc92dc57c822f9743df1f
Amin <ayari.am@campus.lmu.de>
2026-01-03 20:49:27 +0100

    refactoring

--- data/script.js
+++ data/script.js
@@ -4,7 +4,9 @@
 
 
 const state = {
-    colorResult:''
+    colorResult: '',
+    view: 'pick', //pick results
+    hasTriedNext: false
 }
 const stepOneResultsText = document.getElementById('results-step1');
 
@@ -12,45 +14,61 @@ const innerTitle = document.getElementById('inner-title');
 const stepOneResult = document.getElementById('mood-colors');
 const nextBt = document.getElementById('step1-next-bt');
 document.getElementById('step1-next-bt').addEventListener('click', function (e) {
+    if (state.view == 'results') {
+        state.colorResult = null;
+        state.hasTriedNext = false;
+        state.view = 'pick';
+    } else if (state.view == 'pick') {
+        if (state.colorResult == null) {
+            state.hasTriedNext = true;
+        } else {
+            state.view = 'results';
+        }
 
-    console.log(stepOneResult.style.display);
-    if (stepOneResult.style.display == 'none') {
-        handleReset();
-    } else {
-        handleStepOneResults();
 
     }
+    render();
 })
-function handleReset() {
-    stepOneResult.style.display = 'grid';
-    stepOneResultsText.innerHTML= '';
-    nextBt.innerHTML = 'Next';
-    innerTitle.innerHTML = 'Pick a color that matches your current mood:'
-    document.body.style.backgroundImage =`var(--default-bg)`;
-}
-function handleStepOneResults() {
-    if (state.colorResult== ''){
-        innerTitle.innerHTML = 'Pick a color below:';
-        return;
-    }
-    stepOneResult.style.display = 'none';
-    stepOneResultsText.innerHTML= `<h2>${state.colorResult}</h2>`;
 
-    nextBt.innerHTML = 'Redo';
-    innerTitle.innerHTML = 'Your likely mood:'
-    state.colorResult='';
-
-}
 const boxes = document.querySelectorAll('.color-box');
 
 boxes.forEach(box => {
     box.addEventListener('click', () => {
-        console.log('ok');
-        // box.style.background='#fbf8ff';
         document.body.style.background = `${box.style.background}`;
         boxes.forEach(b => b.classList.remove('selected'))
         box.classList.add('selected');
-        state.colorResult= box.getAttribute('data-mood');
+        state.colorResult = box.getAttribute('data-mood');
+        // state.view = 'picked';
 
     });
 });
+
+
+function render() {
+    const tmpState = state.view;
+    if (tmpState == 'pick') {
+        renderPick();
+    } else if (tmpState == 'results') {
+        renderResults();
+    }
+
+}
+
+function renderPick() {
+    innerTitle.innerHTML = state.hasTriedNext ?
+        'Pick a color below:' : 'Pick a color that matches your current mood:';
+
+    stepOneResult.style.display = 'grid';
+    stepOneResultsText.innerHTML = '';
+    nextBt.innerHTML = 'Next';
+    document.body.style.backgroundImage = `var(--default-bg)`;
+    boxes.forEach(b => b.classList.remove('selected'));
+}
+function renderResults() {
+    stepOneResult.style.display = 'none';
+    stepOneResultsText.innerHTML = `<h2>${state.colorResult}</h2>`;
+
+    nextBt.innerHTML = 'Redo';
+    innerTitle.innerHTML = 'Your likely mood:'
+    state.colorResult = null;
+}
